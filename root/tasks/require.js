0c8609863c4cfaaad6b6b7d41bdbdd4c3616a24a
Thomas Wilburn <twilburn@seattletimes.com>
2014-08-13 14:17:41 -0700

    Add common paths to the Require task
    Now you can just define "jquery" "pym" or "angular" as a dependency
    after running the requisite bower install.

--- root/tasks/require.js
+++ root/tasks/require.js
@@ -6,23 +6,37 @@ Builds a source package, starting from src/js/main.js
 
 var r = require("requirejs");
 var shell = require("shelljs");
+var project = require("../project.json");
 
 module.exports = function(grunt) {
 
   grunt.registerTask("amd", "Compile AMD modules to build/main.js", function() {
     var c = this.async();
 
-    //build an optimized app bundle
-    //include almond for resource loading
-    r.optimize({
+    var config = {
       baseUrl: "src/js",
       name: "main",
       include: ["almond.js"],
       out: "build/app.js",
       generateSourceMaps: true,
       preserveLicenseComments: false,
-      optimize: "none"
-    }, c);
+      optimize: "none",
+      //common paths for bower packages
+      //luckily, require won't complain unless we use them
+      paths: {
+        jquery: "lib/jquery/dist/jquery.min",
+        pym: "lib/pym.js/src/pym",
+        angular: "lib/angular/angular.min.js"
+      }
+    };
+
+    for (var key in project.require) {
+      config[key] = project.require[key];
+    }
+
+    //build an optimized app bundle
+    //include almond for resource loading
+    r.optimize(config, c);
   })
 
 };
