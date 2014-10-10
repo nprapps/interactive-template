6b10b90467ed8c409b07ab74a043e82e5b007c77
Thomas Wilburn <news@thomaswilburn.net>
2014-10-09 19:19:05 -0700

    Forgot to conclude async LESS task

--- root/tasks/less.js
+++ root/tasks/less.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
     parser.parse(seed, function(err, tree) {
       var css = tree.toCSS();
       grunt.file.write("build/style.css", css);
+      c();
     });
     
   });
