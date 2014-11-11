e8d6761a27a09ef9234334f6df93200f1afd49cf
Thomas Wilburn <twilburn@seattletimes.com>
2014-11-11 14:36:18 -0800

    Add minification to the output file.

--- root/tasks/bundle.js
+++ root/tasks/bundle.js
@@ -4,19 +4,21 @@ Build a bundled app.js file using browserify
 module.exports = function(grunt) {
 
   var browserify = require("browserify");
-  var exorcist = require("exorcist");
   var fs = require("fs");
 
   grunt.registerTask("bundle", "Build app.js using browserify", function() {
 
     var done = this.async();
 
+    grunt.file.mkdir("build");
+
     var b = browserify({ debug: true });
 
     var output = fs.createWriteStream("build/app.js");
 
     b.add("./src/js/main.js");
-    b.bundle().pipe(exorcist("build/app.js.map")).pipe(output).on("finish", function() {
+    b.plugin("minifyify", { map: "app.js.map", output: "build/app.js.map" });
+    b.bundle().pipe(output).on("finish", function() {
       done();
     });
 
