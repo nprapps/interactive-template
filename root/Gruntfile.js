9a733768bae6f74c89367f8affde7cd8012d1ef7
Thomas Wilburn <news@thomaswilburn.net>
2014-11-20 13:43:33 -0800

    Update Gruntfile in prep for adding Markdown task

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -3,8 +3,9 @@ module.exports = function(grunt) {
   //load tasks
   grunt.loadTasks("./tasks");
 
-  grunt.registerTask("template", "Perform a complete build of data and templates", ["state", "json", "csv", "build"]);
-  grunt.registerTask("static", ["copy", "bundle", "less", "template"]);
+  grunt.registerTask("content", "Load content from data files", ["state", "json", "csv"]);
+  grunt.registerTask("template", "Build HTML from content/templates", ["content", "build"]);
+  grunt.registerTask("static", "Build all files", ["copy", "bundle", "less", "template"]);
   grunt.registerTask("default", ["static", "connect:dev", "watch"]);
 
 };
