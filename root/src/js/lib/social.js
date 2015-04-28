1940b7f7682b1f4fd4fa05eb9391278df76f519c
Thomas Wilburn <news@thomaswilburn.net>
2015-04-28 10:47:48 -0700

    Add Andrew's UTM codes

--- root/src/js/lib/social.js
+++ root/src/js/lib/social.js
@@ -1,12 +1,34 @@
 var Share = require("./share.min.js");
 
-new Share(".share", {
+var addQuery = function(url, query) {
+  var joiner = url.indexOf("?") > -1 ? "&" : "?";
+  return url + joiner + query;
+};
+
+var utm = function(source, medium) {
+  return `utm_source=${source}&utm_medium=${medium || "social"}&utm_campaign=projects`;
+};
+
+var here = window.location.href;
+
+var s = new Share(".share", {
   ui: {
     flyout: "bottom left"
   },
   networks: {
-    email: {
-      description: window.location.href
+    google_plus: {
+      url: addQuery(here, utm("google+"))
+    },
+    twitter: {
+      url: addQuery(here, utm("twitter"))
+    },
+    facebook: {
+      url: addQuery(here, utm("facebook"))
+    },
+    pinterest: {
+      url: addQuery(here, utm("pinterest"))
     }
   }
 });
+
+s.config.email.description += " " + addQuery(here, utm("email_share", "email"));
