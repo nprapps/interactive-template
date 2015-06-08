2c58849ffc4e2e3d932cc30e75564196c21662f9
Audrey Carlsen <acarlsen@seattletimes.com>
2015-06-08 16:48:53 -0700

    banner ad size mappings

--- root/src/js/lib/ads.js
+++ root/src/js/lib/ads.js
@@ -1,11 +1,16 @@
   var tag = window.googletag = window.googletag || { cmd: [], seatimes: { unit: "/81279359/seattletimes.com" } };
   
+  var bannerMapping = { 
+    768: [[728,90]],
+    1: [[300, 50]]
+  };
+
   tag.seatimes.presets = {
-    banner: { sizes: [[728,90]], position: "top" },
+    banner: { sizes: [[728,90]], sizeMapping: bannerMapping, position: "top" },
     square: { sizes: [[300, 250]], position: "right1" },
     tall: { sizes: [[300, 600]], position: "right" },
     right: { sizes: [[300, 600], [300, 250]], position: "right" },
-    bannerBottom: { sizes: [[728,90]], position: "bottom" },
+    bannerBottom: { sizes: [[728,90]], sizeMapping: bannerMapping, position: "bottom" },
     interstitial: { sizes: [[1,1]], position: "inter" }
   };
   
