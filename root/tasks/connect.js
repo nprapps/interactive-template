932999494a4e543c3b7969375194bd1840a7f4b0
Thomas Wilburn <news@thomaswilburn.net>
2015-08-31 12:57:21 -0700

    Add a filter for case sensitivity
    @audreycarlsen Let me know if you see this screw anything up for you. It
    should be relatively stable, but you never know.

--- root/tasks/connect.js
+++ root/tasks/connect.js
@@ -5,6 +5,9 @@ livereload server at some point.
 
 */
 
+var fs = require("fs");
+var path = require("path");
+
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-connect");
@@ -14,7 +17,26 @@ module.exports = function(grunt) {
       dev: {
         options: {
           livereload: true,
-          base: "./build"
+          base: "./build",
+          //middleware to protect against case-insensitive file systems
+          middleware: function(connect, options, ware) {
+            var base = options.base.pop();
+            ware.unshift(function(req, response, next) {
+              var location = path.join(base, req.url);
+              var filename = path.basename(req.url);
+              if (!filename) return next();
+              var dir = path.dirname(location);
+              fs.readdir(dir, function(err, list) {
+                if (list.indexOf(filename) == -1) {
+                  response.statusCode = 404;
+                  response.end();
+                } else {
+                  next();
+                }
+              })
+            });
+            return ware;
+          }
         }
       }
     }
