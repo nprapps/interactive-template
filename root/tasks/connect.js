09d77f68de6320ac509775b7604e247b721528b5
Thomas Wilburn <news@thomaswilburn.net>
2015-09-01 08:59:33 -0700

    Fix a bug where query strings would 404

--- root/tasks/connect.js
+++ root/tasks/connect.js
@@ -7,6 +7,7 @@ livereload server at some point.
 
 var fs = require("fs");
 var path = require("path");
+var url = require("url");
 
 module.exports = function(grunt) {
 
@@ -22,8 +23,9 @@ module.exports = function(grunt) {
           middleware: function(connect, options, ware) {
             var base = options.base.pop();
             ware.unshift(function(req, response, next) {
-              var location = path.join(base, req.url);
-              var filename = path.basename(req.url);
+              var href = url.parse(req.url).pathname;
+              var location = path.join(base, href);
+              var filename = path.basename(href);
               if (!filename) return next();
               var dir = path.dirname(location);
               fs.readdir(dir, function(err, list) {
