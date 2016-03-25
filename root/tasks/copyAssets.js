65fe527bdff47a096bb3b5079ef4f02b13210411
Thomas Wilburn <news@thomaswilburn.net>
2016-03-25 15:57:40 -0700

    Noticed a weird bug popping up with assets

--- root/tasks/copyAssets.js
+++ root/tasks/copyAssets.js
@@ -9,7 +9,10 @@ var shell = require("shelljs");
 module.exports = function(grunt) {
 
   grunt.registerTask("copy", "Copy assets directory", function() {
-    if (grunt.file.exists("src/assets")) shell.cp("-r", "src/assets", "build");
+    if (grunt.file.exists("src/assets")) {
+      if (!grunt.file.exists("build/assets")) shell.mkdir("-p", "build/assets");
+      shell.cp("-r", "src/assets", "build");
+    }
   });
 
 }
