6e1de62f3bd7a9fd829dbe5d870a40fd955f37c6
Thomas Wilburn <news@thomaswilburn.net>
2016-07-11 12:42:08 -0700

    Add colors, .interactive style, preset buckets

--- root/src/js/lib/colors.js
+++ root/src/js/lib/colors.js
@@ -1,5 +1,36 @@
-module.exports = {
-  rgb: (r, g, b) => `rgb(${r}, ${g}, ${b})`,
-  rgba: (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a || 1})`,
-  hsl: (h, s, l) => `hsl(${h}, ${s}%, ${l}%)`
+var rgb = (r, g, b) => `rgb(${r}, ${g}, ${b})`;
+var rgba = (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a || 1})`;
+var hsl = (h, s, l) => `hsl(${h}, ${s}%, ${l}%)`;
+
+var palette = {
+  stLightRed: rgb(229, 175, 155),
+  stDarkRed: rgb(202, 105, 81),
+  stLightOrange: rgb(255, 218, 162),
+  stDarkOrange: rgb(248, 158, 93),
+  stLightGreen: rgb(181, 191, 169),
+  stDarkGreen: rgb(121, 143, 113),
+  stLightBlue: rgb(213, 228, 240),
+  stDarkBlue: rgb(163, 193, 221),
+  stLightPurple: rgb(199, 187, 220),
+  stDarkPurple: rgb(123, 90, 166),
+
+  // Neutral shades
+  dfOffBlack: rgb(35, 31, 32),
+  dfOffWhite: rgb(248, 248, 248),
+  dfFooterGray: rgb(239, 239, 239),
+  dfLightGray: rgb(224, 224, 224),
+  dfBorderGray: rgb(192, 192, 192),
+  dfMiddleGray: rgb(126, 131, 139),
+  dfDarkGray: rgb(66, 70, 72),
+  dfCharcoal: rgb(34, 34, 34),
+
+  // Accent colors
+  dfBlue: rgb(7, 119, 179),
+  dfMaroon: rgb(192, 33, 138),
+  dfOrange: rgb(188, 92, 35),
+  dfOrangeHighlight: rgb(208, 103, 35),
+  dfGoldenrod: rgb(225, 135, 39),
+  dfNavy: rgb(43, 77, 112)
 };
+
+module.exports = { rgb, rgba, hsl, palette };
