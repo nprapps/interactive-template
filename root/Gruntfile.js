2a7528005bd31a2123629b0aed803740008d360b
Audrey Carlsen <acarlsen@seattletimes.com>
2016-09-15 10:52:06 -0700

    Adds task to build without assets

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -7,5 +7,5 @@ module.exports = function(grunt) {
   grunt.registerTask("template", "Build HTML from content/templates", ["content", "build"]);
   grunt.registerTask("static", "Build all files", ["copy", "bundle", "less", "template"]);
   grunt.registerTask("default", ["clean", "static", "connect:dev", "watch"]);
-
+  grunt.registerTask("quick", "Build without assets", ["clean", "bundle", "less", "template"]);
 };
