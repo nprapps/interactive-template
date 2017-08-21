c03948e0498e23a3435c2d2757fa0676dadad419
Thomas Wilburn <news@thomaswilburn.net>
2017-08-21 13:39:18 -0700

    Add a limit for closest() to chain upward

--- root/src/js/lib/closest.js
+++ root/src/js/lib/closest.js
@@ -1,6 +1,6 @@
 var matches = require("./prefixed").matches;
 
-module.exports = function(element, selector) {
-  while (element && !element[matches](selector)) element = element.parentNode;
+module.exports = function(element, selector, limit = document.body) {
+  while (element && !element[matches](selector) && element != limit) element = element.parentNode;
   return element;
 };
--- root/src/js/lib/delegate.js
+++ root/src/js/lib/delegate.js
@@ -2,7 +2,7 @@ var closest = require("./closest");
 
 var delegate = function(root, event, selector, callback) {
   root.addEventListener(event, function(e) {
-    var matching = closest(e.target, selector);
+    var matching = closest(e.target, selector, root);
     if (matching) {
       callback.call(matching, e);
     }
