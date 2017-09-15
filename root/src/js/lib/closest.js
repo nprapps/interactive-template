4de515f1a778ce42c5dff05f4a57c0dc1c734dec
Thomas Wilburn <news@thomaswilburn.net>
2017-09-15 14:15:06 -0700

    Closest should return null when hitting the limit

--- root/src/js/lib/closest.js
+++ root/src/js/lib/closest.js
@@ -1,6 +1,9 @@
 var matches = require("./prefixed").matches;
 
 module.exports = function(element, selector, limit = document.body) {
-  while (element && !element[matches](selector) && element != limit) element = element.parentNode;
+  while (element && !element[matches](selector) && element != limit) {
+    element = element.parentNode;
+  }
+  if (element == limit) return null;
   return element;
 };
