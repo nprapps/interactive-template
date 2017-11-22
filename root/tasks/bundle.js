dfa80240167d68fb88ef1bc7d6b857359bd62ed2
Thomas Wilburn <news@thomaswilburn.net>
2017-11-22 09:28:52 -0800

    Flat pack modules for speed now

--- root/tasks/bundle.js
+++ root/tasks/bundle.js
@@ -21,6 +21,7 @@ module.exports = function(grunt) {
 
     async.forEachOf(seeds, function(dest, src, c) {
       var b = browserify({ debug: mode == "dev" });
+      b.plugin(require("browser-pack-flat/plugin"));
       b.transform(babel, { global: true, presets: [
         ["env", {
           targets: { browsers: ["ie >= 10", "safari >= 8"]}
