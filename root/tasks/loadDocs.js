abb9acda3abc1b7928e77c5746d4600b51754ac2
Thomas Wilburn <twilburn@npr.org>
2018-11-12 16:49:25 -0500

    Added a docs task

---
+++ root/tasks/loadDocs.js
@@ -0,0 +1,34 @@
+var { google } = require("googleapis");
+var async = require("async");
+var os = require("os");
+var path = require("path");
+
+module.exports = function(grunt) {
+
+  grunt.registerTask("docs", "Load Google Docs into the data folder", function() {
+
+    var config = grunt.file.readJSON("project.json");
+    try {
+      var access_token = grunt.file.read(path.join(os.homedir(), ".google_oauth_token"));
+      auth = new google.auth.OAuth2(process.env.GOOGLE_OAUTH_CLIENT_ID, process.env.GOOGLE_OAUTH_CONSUMER_SECRET);
+      auth.setCredentials({ access_token });
+    } catch (err) {
+      console.log(err);
+      return grunt.fail.warn("Couldn't load access token for Docs, try running `grunt google-auth`");
+    }
+
+    var done = this.async();
+
+    var drive = google.drive({ auth, version: "v3" });
+
+    async.each(config.docs, async function(fileId) {
+      var meta = await drive.files.get({ fileId });
+      var name = meta.data.name.replace(/\s+/g, "_") + ".docs.txt";
+      var body = await drive.files.export({ fileId, mimeType: "text/plain" });
+      var text = body.data.replace(/\r\n/g, "\n");
+      console.log(`Writing document as data/${name}`);
+      grunt.file.write(path.join("data", name), text);
+    }, done);
+
+  });
+}
