c3cfa28769159b2d42ff7a7c6af644b2ec1a18de
Thomas Wilburn <twilburn@npr.org>
2019-04-30 12:58:50 -0500

    Update to v4 API

--- root/tasks/loadSheets.js
+++ root/tasks/loadSheets.js
@@ -4,17 +4,14 @@ Uses the Google Sheets API to pull data from Sheets and load it onto shared
 state. Writes the data out to JSON for later reference. Does not currently
 check for existing data to merge--it does a fresh pull every time.
 
-Sheets must be both A) shared publicly and B) "published to web" before they
-can be accessed by this task.
-
 */
 
 var project = require("../project.json");
 var async = require("async");
-var sheets = require("google-spreadsheets");
 var os = require("os");
 var path = require("path");
 var { google } = require("googleapis");
+var api = google.sheets("v4");
 
 var { authenticate } = require("./googleauth");
 
@@ -45,7 +42,7 @@ var cast = function(str) {
 
 module.exports = function(grunt) {
 
-  grunt.registerTask("sheets", "Downloads from Google Sheets -> JSON", function() {
+  grunt.registerTask("sheets", "Downloads from Google Sheets -> JSON", async function() {
 
     var auth = null;
     try {
@@ -62,67 +59,40 @@ module.exports = function(grunt) {
 
     var done = this.async();
 
-    async.each(sheetKeys, function(key, bookDone) {
-
-      sheets({ key, auth }, function(err, book) {
-        if (err || !book) {
-          grunt.fail.warn("Unable to access book for " + key + ", is it 'published' in Sheets?");
-          return bookDone();
-        }
-        //download each worksheet
-        async.each(book.worksheets, function(page, pageDone) {
-          page.cells({ key: key, worksheet: page.id }, function(err, cells) {
-            if (err) {
-              console.log(err);
-              grunt.fail.warn("Couldn't load sheet " + page.id + " for " + book.title);
-              return pageDone();
-            }
-            cells = cells.cells;
-            
-            //get the header row
-            var rowNumbers = Object.keys(cells);
-            var headerNumber = rowNumbers.shift();
-            var headers = cells[headerNumber];
-            var isKeyed = false;
-            for (var cell in headers) {
-              headers[cell] = headers[cell].value;
-              if (headers[cell] == "key") {
-                isKeyed = true;
-              }
-            }
-
-            var output = isKeyed ? {} : [];
-            
-            //create the rows
-            rowNumbers.forEach(function(rowNum) {
-              var line = cells[rowNum];
-              var columnNumbers = Object.keys(line);
-              var row = {};
-              var encountered = [];
-              columnNumbers.forEach(function(colNum) {
-                var value = line[colNum];
-                if (!value) return;
-                var key = headers[colNum];
-                if (!key) return;
-                encountered.push(key);
-                row[key] = cast(value);
-              });
-              if (!encountered.length) return;
-              if (isKeyed) {
-                output[row.key] = row.value && encountered.length == 2 ? row.value : row;
-                delete row.key;
-              } else {
-                output.push(row);
-              }
-            });
-
-            var filename = "data/" + camelCase(page.title) + ".sheet.json";
-            grunt.file.write(filename, JSON.stringify(output, null, 2));
-            pageDone();
+    for (var spreadsheetId of sheetKeys) {
+      var book = (await api.spreadsheets.get({ auth, spreadsheetId })).data;
+      var { sheets, spreadsheetId } = book;
+      for (var sheet of sheets) {
+        var response = await api.spreadsheets.values.get({
+          auth,
+          spreadsheetId,
+          range: `${sheet.properties.title}!A:AAA`,
+          majorDimension: "ROWS"
+        });
+        var { values } = response.data;
+        var header = values.shift();
+        var isKeyed = header.indexOf("key") > -1;
+        var isValued = header.indexOf("value") > -1;
+        var out = isKeyed ? {} : [];
+        for (var row of values) {
+          var obj = {};
+          row.forEach(function(value, i) {
+            var key = header[i];
+            obj[key] = cast(value);
           });
-        }, bookDone);
-      });
-    }, done);
+          if (isKeyed) {
+            out[obj.key] = isValued ? obj.value : obj;
+          } else {
+            out.push(obj);
+          }
+        }
+        var filename = `data/${sheet.properties.title.replace(/\s+/g, "_")}.sheet.json`;
+        console.log(`Saving sheet to ${filename}`);
+        grunt.file.write(filename, JSON.stringify(out, null, 2));
+      }
+    }
+
+    done();
 
   });
 
