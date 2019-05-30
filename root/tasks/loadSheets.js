0061d7e879180f99a028cf7c5f51ddab847a334c
Thomas Wilburn <twilburn@npr.org>
2019-05-30 17:58:56 -0500

    Skip _prefixed sheets

--- root/tasks/loadSheets.js
+++ root/tasks/loadSheets.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
       var book = (await api.spreadsheets.get({ auth, spreadsheetId })).data;
       var { sheets, spreadsheetId } = book;
       for (var sheet of sheets) {
+        if (sheet.properties.title[0] == "_") continue;
         var response = await api.spreadsheets.values.get({
           auth,
           spreadsheetId,
