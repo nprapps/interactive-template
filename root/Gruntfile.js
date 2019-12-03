4fa5b8493663b5f46aeb10ef61883d21f6e41679
thomaswilburn <twilburn@npr.org>
2019-12-03 08:13:29 -0600

    Gruntfile updates

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,11 +1,35 @@
 module.exports = function(grunt) {
-
   //load tasks
   grunt.loadTasks("./tasks");
 
-  grunt.registerTask("content", "Load content from data files", ["state", "json", "csv", "markdown", "archieml"]);
-  grunt.registerTask("template", "Build HTML from content/templates", ["content", "build"]);
-  grunt.registerTask("static", "Build all files", ["copy", "bundle", "less", "template"]);
-  grunt.registerTask("default", ["clean", "static", "connect:dev", "watch"]);
-  grunt.registerTask("quick", "Build without assets", ["clean", "bundle", "less", "template"]);
+  grunt.registerTask("update", "Download content from remote services", function(target = stage) {
+    grunt.task.run(["sheets", "docs", `sync:${target}`]);
+  });
+  grunt.registerTask("content", "Load content from data files", [
+    "state",
+    "json",
+    "csv",
+    "markdown",
+    "archieml",
+    "shelve",
+    "validate"
+  ]);
+  grunt.registerTask("template", "Build HTML from content/templates", [
+    "content",
+    "build"
+  ]);
+  grunt.registerTask("static", "Build all files", [
+    "copy",
+    "bundle",
+    "less",
+    "template"
+  ]);
+  grunt.registerTask("quick", "Build without assets", [
+    "clean",
+    "bundle",
+    "less",
+    "template"
+  ]);
+  grunt.registerTask("serve", "Start the dev server", ["connect:dev", "watch"]);
+  grunt.registerTask("default", ["clean", "static", "serve"]);
 };
